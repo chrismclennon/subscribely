9e6aa9d481a0ebf0e45926ba6559e73fc72676de
Matt Groot <matt@ownlocal.com>
2016-10-22 13:37:55 -0700

    Revert "Spring cleaning"
    This reverts commit c345883e084a5c137c6eef75d4547d136bde3e9e.

--- subscribely/subscribely.py
+++ subscribely/subscribely.py
@@ -67,3 +67,23 @@ def disable_subscription():
     # TODO: authenticate and disable subscription
     flash('Subscription successfully disabled.')
     return redirect(url_for('dashboard'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('dashboard'))
