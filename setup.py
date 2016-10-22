45874b150eab8a80f987d0a016eadc8a1af78c4e
Matt Groot <matt@ownlocal.com>
2016-10-22 11:08:49 -0700

    flaskr skeleton

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='subscribely',
+    packages=['subscribely'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
