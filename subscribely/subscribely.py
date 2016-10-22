67e7ae4bb16181772e2b80093db5e6579211ffe9
Chris McLennon <mclennon@indeed.com>
2016-10-22 13:16:04 -0700

    Spring cleaning

--- subscribely/subscribely.py
+++ subscribely/subscribely.py
@@ -14,11 +14,12 @@ app.config.update(dict(
 ))
 app.config.from_envvar('SUBSCRIBELY_SETTINGS', silent=True)
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+# DATABASE
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
 
 def init_db():
     """Initializes the database."""
@@ -26,12 +27,6 @@ def init_db():
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -41,12 +36,19 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+# ROUTING
 @app.route('/')
 def dashboard():
     db = get_db()
@@ -64,17 +66,6 @@ def enable_subscription():
 def disable_subscription():
     # TODO: authenticate and disable subscription
     flash('Subscription successfully disabled.')
-    return redirect(url_for('dashboard'))
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
     return redirect(url_for('dashboard'))
 
 @app.route('/login', methods=['GET', 'POST'])
