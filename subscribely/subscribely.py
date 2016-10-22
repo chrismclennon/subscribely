6924b61e4755bc7caa91d22d2e2c9eab65b284a5
Chris McLennon <mclennon@indeed.com>
2016-10-22 13:05:13 -0700

    Database

--- subscribely/subscribely.py
+++ subscribely/subscribely.py
@@ -1,56 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-    Flaskr
-    ~~~~~~
-
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-
-    :copyright: (c) 2015 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
-"""
-
 import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-
-# create our little application :)
 app = Flask(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'subscribely.db'),
     DEBUG=True,
-    SECRET_KEY='development key',
+    SECRET_KEY='uoOBAh6F4yKJQXXQgjLSYy8bxuyri58F',
     USERNAME='admin',
     PASSWORD='default'
 ))
 app.config.from_envvar('SUBSCRIBELY_SETTINGS', silent=True)
-
 
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
-
-
-def init_db():
-    """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -60,13 +29,23 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
 
 @app.route('/')
 def show_entries():
@@ -74,7 +53,6 @@ def show_entries():
     cur = db.execute('select title, text from entries order by id desc')
     subscriptions = cur.fetchall()
     return render_template('dashboard.html', subscriptions=subscriptions)
-
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -86,7 +64,6 @@ def add_entry():
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('dashboard'))
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -101,7 +78,6 @@ def login():
             flash('You were logged in')
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
-
 
 @app.route('/logout')
 def logout():
