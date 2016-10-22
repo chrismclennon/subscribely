370fb3120251991f048376fc83d1828234ff63ee
Chris McLennon <mclennon@indeed.com>
2016-10-22 15:59:16 -0700

    Cleanup

--- subscribely/schema.sql
+++ subscribely/schema.sql
@@ -2,15 +2,10 @@ CREATE TABLE IF NOT EXISTS users (
   user_id INT PRIMARY KEY,
   name TEXT
 );
-CREATE TABLE IF NOT EXISTS user_billing (
-  -- Currently assumes: 1 credit card per customer
-  user_id INT REFERENCES users(user_id),
-  name_on_card TEXT,
-  credit_card_number TEXT,
-  expiration_month INT,
-  expiration_year INT,
-  security_code INT,
-  zip_code TEXT
+CREATE TABLE IF NOT EXISTS user_modo (
+  user_id INT PRIMARY KEY REFERENCES users(user_id),
+  modo_account_id TEXT,
+  is_active BOOLEAN
 );
 CREATE TABLE IF NOT EXISTS services (
   service_id INT PRIMARY KEY,
@@ -23,4 +18,11 @@ CREATE TABLE IF NOT EXISTS user_subscriptions (
   activity_ts TIMESTAMP,
   next_charge_dt DATE,
   next_charge_amt DOUBLE PRECISION
+);
+CREATE TABLE IF NOT EXISTS transaction_history (
+  transaction_id INT PRIMARY KEY,
+  user_id INT REFERENCES users(user_id),
+  service_id INT REFERENCES services(service_id),
+  transaction_ts TIMESTAMP,
+  transaction_amt DOUBLE PRECISION
 );
