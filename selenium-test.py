e6e8733f00a9620a6c9e62134c745f892b10a101
Matt Groot <matt@ownlocal.com>
2016-10-22 21:06:34 -0700

    fucking selenium cannot into credit cards

--- selenium-test.py
+++ selenium-test.py
@@ -1,5 +1,51 @@
 import os
+import time
 from selenium import webdriver
+
+def select(element, option_text):
+    for option in element.find_elements_by_tag_name('option'):
+        if option.text == option_text:
+            option.click()
+            break
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+
+driver.get("https://accounts.spotify.com/en-US/login")
+driver.find_element_by_id("login-username").send_keys("ktperryfan007")
+driver.find_element_by_id("login-password").send_keys("tswifty")
+
+login_button = driver.find_element_by_css_selector("button")
+login_button.click()
+
+success_notification = driver.find_element_by_xpath("//*[contains(text(), 'You are logged in as ktperryfan007.')]")
+print(success_notification.text)
+
+driver.get("https://www.spotify.com/us/purchase/panel/#__main-pci-credit-card")
+time.sleep(10)
+print(len(driver.window_handles))
+inputs = driver.find_elements_by_tag_name('input')
+print(len(inputs))
+for e in inputs:
+    print("id: " + e.get_attribute('id'))
+    print("class: " + e.get_attribute('class'))
+payment_form = driver.find_element_by_xpath("//*[@data-encrypted-name='number']")
+driver.find_element_by_id("payment-form").send_keys("1111222233334444")
+#driver.find_element_by_css_selector("#cardnumber").send_keys("1111222233334444")
+select(driver.find_element_by_id("expiry-month"), "January")
+select(driver.find_element_by_id("expiry-month"), "20")
+driver.find_element_by_id("security-code").send_keys("123")
+driver.find_element_by_id("zip-code").send_keys("78787")
+
+payment_button = driver.find_element_by_xpath("//*[contains(text(), 'Start my Spotify Premium')]")
+payment_button.click()
+
+error_container = driver.find_element_by_xpath("//*[@class='error-container']")
+error_notifications = error_container.find_elements_by_tag_name('li')
+print("cc_entry errors: " + len(error_notifications))
+driver.quit()
+
+
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
